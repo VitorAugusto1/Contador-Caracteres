da54e877d04ecba39c74d14e128f75d8531994b6
Vitor <vitornm9@gmail.com>
2026-01-21 23:23:07 -0300

    Commit 1.1

--- contador-caracteres/src/main/java/caracterescontador/Main.java
+++ contador-caracteres/src/main/java/caracterescontador/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Digite uma frase: ");
+            System.out.print("Digite a frase: ");
             String frase = scanner.nextLine();
             int contador = 0;
             int numeroDigitos = 0;

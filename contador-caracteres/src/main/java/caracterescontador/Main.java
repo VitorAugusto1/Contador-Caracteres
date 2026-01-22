00cb008840632f327988569785bafdaa6cb4698e
Vitor <vitornm9@gmail.com>
2026-01-21 23:10:29 -0300

    Contador 1.0

---
+++ contador-caracteres/src/main/java/caracterescontador/Main.java
@@ -0,0 +1,27 @@
+package caracterescontador;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite uma frase: ");
+            String frase = scanner.nextLine();
+            int contador = 0;
+            int numeroDigitos = 0;
+            for (int i = 0; i < frase.length(); i++) {
+                char c = frase.charAt(i);
+                if (c != ' ') {
+                    contador++;
+                }
+                if (Character.isDigit(c)) {
+                    numeroDigitos++;
+                }
+            }
+            String mensagem = "A frase tem " + contador + " caracteres (sem contar espaços)";
+            if (numeroDigitos > 0) {
+                mensagem += " e " + numeroDigitos + " dígitos.";
+            }
+            System.out.println(mensagem);
+        }
+    }
+}

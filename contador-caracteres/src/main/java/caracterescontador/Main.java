d77dff9e768a9467a169c6b5304bddfd46670840
Vitor <vitornm9@gmail.com>
2026-01-22 21:41:13 -0300

    Contador 1.3

--- contador-caracteres/src/main/java/caracterescontador/Main.java
+++ contador-caracteres/src/main/java/caracterescontador/Main.java
@@ -6,20 +6,34 @@ public class Main {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Digite a frase: ");
             String frase = scanner.nextLine();
-            int contador = 0;
+            int contadorLetras = 0;
+            int contadorEspaços = 0;
             int numeroDigitos = 0;
+            int contadorEspecial = 0;
             for (int i = 0; i < frase.length(); i++) {
                 char c = frase.charAt(i);
-                if (c != ' ') {
-                    contador++;
+                if (Character.isLetter(c)) {
+                    contadorLetras++;
                 }
                 if (Character.isDigit(c)) {
                     numeroDigitos++;
                 }
-            }
-            String mensagem = "A frase tem " + contador + " caracteres (sem contar espaços)";
+                if (!Character.isLetterOrDigit(c) && c != ' ') {
+                    contadorEspecial++;
+                }
+                if (c == ' ') {
+                    contadorEspaços++;
+                }
+            }   
+            String mensagem = "A frase tem " + contadorLetras + " letras";
             if (numeroDigitos > 0) {
-                mensagem += " e " + numeroDigitos + " dígitos.";
+                mensagem += ", " + numeroDigitos + " dígitos";
+            }
+            if (contadorEspecial > 0) {
+                mensagem += ", " + contadorEspecial + " caracteres especiais";
+            }
+            if (contadorEspaços > 0 ) {
+                mensagem += " e " + contadorEspaços + " espaços";
             }
             System.out.println(mensagem);
         }
